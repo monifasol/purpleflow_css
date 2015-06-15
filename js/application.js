3746c80035b68c0f27ab1b3ce2ebeb639b0fa8c7
monifasol <moni.sm@gmail.com>
2015-06-15 03:10:36 +0200

    Crossbrowsing improvements

--- js/application.js
+++ js/application.js
@@ -8,20 +8,9 @@ $(document).ready(function() {
     $(".close-menu").click(function() {
         $(".menu").removeClass("is-active");
     });
-
+    
     $(".dropdown").click(function() {
         $(this).toggleClass("is-active");
     });
 
 });
-
-function is_on_screen(el){
-	var bottom_of_object = $(el).offset().top + $(el).outerHeight();
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-    if( bottom_of_window > bottom_of_object ){
-        return true;
-    } else {
-    	return false;
-    }
-}
-
